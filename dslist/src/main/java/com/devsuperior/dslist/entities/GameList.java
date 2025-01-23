2acd442ccd30b025e6a9f657be3cb10ea3ba16da
Mario Sergio <mario.junior23@univille.br>
2025-01-23 19:47:39 -0300

    GET lists

--- dslist/src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 
 @Entity
-@Table(name = "tb_game_list")
+@Table(name ="tb_game_list")
 public class GameList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
